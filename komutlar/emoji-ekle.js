14522972e2376e6487531315a7b9f2705741c444
Glitch (v12-kayit-altyapi) <none>
2021-07-28 13:06:01 +0000

    🎐👱 Checkpoint ./komutlar/emoji-ekle.js:29605485/1416

---
+++ komutlar/emoji-ekle.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+
+exports.run = (client, message, args) => {
+  if(!message.member.roles.cache.has("808297886404182016") && !message.member.hasPermission("ADMINISTRATOR"))
+    return message.channel.send(
+      "Bu komutu kullanabilmek için **Emojileri yönet** yetkisine sahip olmalısınız"
+    );
+  let erenlink = args[0];
+  let erenisim = args[1];
+  let guild = message.guild;
+  if (!erenlink)
+    return message.channel.send("Emojinin alınacağı linki girmelisin :warning:");
+  if (!erenisim) return message.channel.send("Emojinin ismini belirlemedin :warning:");
+
+  let embed = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setAuthor("Emoji Eklendi", message.guild.iconURL)
+    .setDescription(` **${erenisim} İsmiyle Yeni Bir Emoji Oluşturuldu.**`)
+    .setFooter(`Komutu kullanan yetkili : ${message.author.username}`);
+
+  guild
+    .emojis.create(`${erenlink}`, `${erenisim}`)
+    .then(emoji => message.channel.send(embed));
+  message.react("✅").catch(console.error);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ["addemoji", "emojioluştur", "ee"],
+  permLevel: 0
+};
+exports.help = {
+  name: "emojiekle",
+  description: "Sunucuya emoji eklersiniz",
+  usage: "emojiekle <link> <isim>"
+};
