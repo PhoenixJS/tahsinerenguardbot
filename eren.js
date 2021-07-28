82d65c183973d07f6a8f3182f67c9227377b6816
Glitch (v12-kayit-altyapi) <none>
2021-07-28 13:17:38 +0000

    🤗😹 Checkpoint ./komutlar/küfür-engel.js:29605485/906 ./eren.js:29605485/1901 ./.glitch-assets:29605485/481

--- eren.js
+++ eren.js
@@ -180,3 +180,100 @@ member.guild.members.cache.get(member.id).ban({reason: `yeni hesap olma`})
 member.guild.owner.send(`${moment.utc(erenskullanici .createdAt).format('DD/MM/YYYY')} tarihinde yeni kurulmuş ${member.user.tag} isimli bir hesabı banladım.`)
 });
 /////yeni hessp ban//
+//-------------------- Küfür Engel Sistemi --------------------//
+
+client.on("message", async msg => {
+  const i = await db.fetch(`${msg.guild.id}.kufur`);
+  if (i) {
+    const kufur = [
+      "oç",
+      "amk",
+      "ananı sikiyim",
+      "ananıskm",
+      "piç",
+      "amk",
+      "amsk",
+      "sikim",
+      "sikiyim",
+      "orospu çocuğu",
+      "piç kurusu",
+      "kahpe",
+      "orospu",
+      "mal",
+      "sik",
+      "yarrak",
+      "am",
+      "amcık",
+      "amık",
+      "yarram",
+      "sikimi ye",
+      "mk",
+      "mq",
+      "aq",
+      "ak",
+      "amq"
+    ];
+    if (kufur.some(word => msg.content.includes(word))) {
+      try {
+        if (!msg.member.permissions.has("BAN_MEMBERS")) {
+          msg.delete();
+
+          return msg
+            .reply("Heey! Küfür Yasak.")
+            .then(wiskyx => wiskyx.delete({ timeout: 5000 }));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
+
+client.on("messageUpdate", async msg => {
+  const i = db.fetch(`${msg.guild.id}.kufur`);
+  if (i) {
+    const kufur = [
+      "oç",
+      "amk",
+      "ananı sikiyim",
+      "ananıskm",
+      "piç",
+      "amk",
+      "amsk",
+      "sikim",
+      "sikiyim",
+      "orospu çocuğu",
+      "piç kurusu",
+      "kahpe",
+      "orospu",
+      "mal",
+      "sik",
+      "yarrak",
+      "am",
+      "amcık",
+      "amık",
+      "yarram",
+      "sikimi ye",
+      "mk",
+      "mq",
+      "aq",
+      "ak",
+      "amq"
+    ];
+    if (kufur.some(word => msg.content.includes(word))) {
+      try {
+        if (!msg.member.permissions.has("BAN_MEMBERS")) {
+          msg.delete();
+
+          return msg
+            .reply("Yakaladım Seni! Küfür Yasak.")
+            .then(wiskyx => wiskyx.delete({ timeout: 5000 }));
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+  if (!i) return;
+});
